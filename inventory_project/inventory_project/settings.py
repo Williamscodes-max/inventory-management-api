6cd0eb6c2c37dd5fe7d4f7acb8407db728263b58
williams alayode <walayode@gmail.com>
2026-02-28 20:20:29 +0100

    Fix requirements.txt location

--- inventory_project/inventory_project/settings.py
+++ inventory_project/inventory_project/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x@))=clqfhxv-j4*zea!5fd@fw7&!+*josxi5e7&^*3))t1&yq'
+# SECRET_KEY = 'django-insecure-x@))=clqfhxv-j4*zea!5fd@fw7&!+*josxi5e7&^*3))t1&yq'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+import os
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
